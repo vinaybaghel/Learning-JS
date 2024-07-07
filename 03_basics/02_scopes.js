64fc9d1cf50eb26af4ea21f838cb65708fe4a5f4
vinaybaghel <73424735+vinaybaghel@users.noreply.github.com>
2024-07-07 13:13:17 +0000

    hoisting in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "Vinay"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "vinay"
+    if (username === "vinay") {
         const website = " youtube"
         // console.log(username + website);
     }
