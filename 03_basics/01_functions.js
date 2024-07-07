8b7667025de828e503bf1d694a8b287304bae04a
vinaybaghel <73424735+vinaybaghel@users.noreply.github.com>
2024-07-07 10:57:20 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,34 @@ function loginUserMessage(username = "sam") {
 }
 
 //console.log(loginUserMessage("Vinay"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1){
+        return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username : "Vinay",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+//const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue([200,300,500]));
